64fb3cc18f1613384eeb77ab6931872e5b797592
vannelo <vannelo@vannelos-Laptop.local>
2023-07-17 19:21:18 -0600

    Add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, { PropsWithChildren } from "react";
 import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Nav />
       {children}
+      <Footer />
     </>
   );
 };
