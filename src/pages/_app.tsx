30460b44a8948406a706ef8d9d86f6bded907fc5
vannelo <vannelo@vannelos-Laptop.local>
2023-07-17 19:20:02 -0600

    Add lightbox

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import "./globals.css";
+
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { Outfit } from "next/font/google";
 import { IntlProvider } from "react-intl";
 import locales from "../locales/es-MX.json";
 import Layout from "@/components/Layout/Layout";
 import { ReduxProvider } from "@/redux/provider";
+import { initLightboxJS } from "lightbox.js-react";
+import "lightbox.js-react/dist/index.css";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initLightboxJS("7B78-0C19-DA25-B7A9", "individual");
+  }, []);
+
   return (
     <ReduxProvider>
       <IntlProvider messages={locales} locale="es-MX" defaultLocale="es-MX">
